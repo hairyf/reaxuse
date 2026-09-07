067f839a23e754bf535510780ec43239c9e3603f
hairyf <wwu710632@gmail.com>
2026-09-08 02:26:50 +0800

    feat(core): add useBattery (#1)

---
+++ packages/core/useBattery/demo.tsx
@@ -0,0 +1,28 @@
+import { useBattery } from '@reaxuse/core'
+
+export default function UseBatteryDemo() {
+  const battery = useBattery()
+
+  return (
+    <div>
+      {battery.isSupported
+        ? (
+            <div>
+              <p>
+                Is Charging:
+                {battery.charging ? 'Yes' : 'No'}
+              </p>
+              <p>
+                Battery Level:
+                {(battery.level * 100).toFixed(0)}
+                %
+              </p>
+            </div>
+          )
+        : (
+            <div>Battery API not supported</div>
+          )}
+      <pre lang="yaml">{JSON.stringify(battery, null, 2)}</pre>
+    </div>
+  )
+}
