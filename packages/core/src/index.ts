d87fc30e7743565bcfdc126819470985aa454309
hairyf <wwu710632@gmail.com>
2026-09-07 11:38:04 +0800

    feat(core): add useAsyncState (#78)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -12,7 +12,7 @@
 // export * from './useActiveElement'
 // export * from './useAnimate'
 export * from './useAsyncQueue'
-// export * from './useAsyncState'
+export * from './useAsyncState'
 // export * from './useBase64'
 // export * from './useBattery'
 // export * from './useBluetooth'
