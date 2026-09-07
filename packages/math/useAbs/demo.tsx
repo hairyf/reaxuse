c48ca9ab0e7489e2024557b8a39a5ece92f939c2
hairyf <wwu710632@gmail.com>
2026-09-07 14:58:52 +0800

    feat(math): add useAbs (#62)

---
+++ packages/math/useAbs/demo.tsx
@@ -0,0 +1,32 @@
+import { useAbs } from '@reaxuse/math'
+import { useState } from 'react'
+
+export default function UseAbsDemo() {
+  const [value, setValue] = useState(-23)
+  const result = useAbs(value)
+
+  return (
+    <div>
+      <div>
+        <input
+          id="input"
+          type="range"
+          min={-100}
+          max={100}
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
+      </div>
+      <div>
+        value:
+        {' '}
+        {value}
+      </div>
+      <div>
+        abs:
+        {' '}
+        <strong>{String(result)}</strong>
+      </div>
+    </div>
+  )
+}
