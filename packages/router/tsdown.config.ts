ca91f1a2d5d766a1d8a036d1541fbd598540de04
hairyf <wwu710632@gmail.com>
2026-09-10 13:14:39 +0800

    chore: remove the unused @reaxuse/router package

--- packages/router/tsdown.config.ts
+++
@@ -1,6 +0,0 @@
-import { packages } from '../../meta/packages.ts'
-import { createTsDownConfig } from '../../tsdown.config.ts'
-
-export default createTsDownConfig(
-  packages.find(pkg => pkg.name === 'router')!,
-)
